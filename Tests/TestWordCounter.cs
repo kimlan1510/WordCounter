74bd7824935c2a531570b80a4c3a6eddf7467e1c
kimlan nguyen <kimlan1510@yahoo.com>
2017-06-08 18:35:52 -0700

    Pass test 1 to check if method can get input from users

--- Tests/TestWordCounter.cs
+++ Tests/TestWordCounter.cs
@@ -6,48 +6,48 @@ namespace WordCounter.Objects
   public class TestWord
   {
     [Fact]
-    public void Test1_GetUserInput_ReturnFalse()
+    public void Test1_GetUserInput_ReturnTrue()
     {
       //Arrange
+      string testWord = "hello";
       RepeatCounter newCount = new RepeatCounter("hello", "hello world");
 
       //Act
-      string word = newCount.GetWord();
-      string sentence = newCount.GetSentence();
+      string resultWord = newCount.GetWord();
 
       //Assert
-      Assert.Equal(word, sentence);
+      Assert.Equal(testWord, resultWord);
     }
 
-    [Fact]
-    public void Test2_SplitSentenceIntoWords_ReturnFalse()
-    {
-      //Arrange
-      RepeatCounter newCount = new RepeatCounter("hello", "hello world");
-      string [] word = new string[]{"hello"};
-
-      //Act
-      string sentence = newCount.GetSentence();
-      string[] wordArray = newCount.SplitSentence(sentence);
-
-      //Assert
-      Assert.Equal(word, wordArray);
-    }
-
-    [Fact]
-    public void Test3_CountWord_ReturnFalse()
-    {
-      //Arrange
-      RepeatCounter newCount = new RepeatCounter("hello", "hello world");
-
-      //Act
-      string word = newCount.GetWord();
-      string sentence = newCount.GetSentence();
-      string[] wordArray = newCount.SplitSentence(sentence);
-      int count = newCount.CountRepeats(word, wordArray);
-
-      //Assert
-      Assert.Equal(0, count);
-    }
+    // [Fact]
+    // public void Test2_SplitSentenceIntoWords_ReturnTrue()
+    // {
+    //   //Arrange
+    //   RepeatCounter newCount = new RepeatCounter("hello", "hello world");
+    //   string [] word = new string[]{"hello", "world"};
+    //
+    //   //Act
+    //   string sentence = newCount.GetSentence();
+    //   string[] wordArray = newCount.SplitSentence(sentence);
+    //
+    //   //Assert
+    //   Assert.Equal(word, wordArray);
+    // }
+    //
+    // [Fact]
+    // public void Test3_CountWord_ReturnFalse()
+    // {
+    //   //Arrange
+    //   RepeatCounter newCount = new RepeatCounter("hello", "hello world");
+    //
+    //   //Act
+    //   string word = newCount.GetWord();
+    //   string sentence = newCount.GetSentence();
+    //   string[] wordArray = newCount.SplitSentence(sentence);
+    //   int count = newCount.CountRepeats(word, wordArray);
+    //
+    //   //Assert
+    //   Assert.Equal(0, count);
+    // }
   }
 }
